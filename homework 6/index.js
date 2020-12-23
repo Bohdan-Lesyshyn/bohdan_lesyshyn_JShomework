308b59bae9bec032a291207a33cc8241561f184d
Bohdan_Lesyshyn <amakhasla2@gmail.com>
2020-12-23 20:55:09 +0200

    homework

---
+++ homework 6/index.js
@@ -0,0 +1,37 @@
+// const findLongestWord = function(string) {
+//     let str = string.split(" ")
+//     let longest = 0;
+//     let word = null;
+//     for(let i = 0; i<str.length; i+=1){
+//        if(longest < str[i].length){
+//           longest = str[i].length;
+//           word = str[i];
+//        }
+//     }
+//     return word;
+//   };
+ 
+//   /*
+//    * Вызовы функции для проверки работоспособности твоей реализации.
+//    */
+//   console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+  
+//   console.log(findLongestWord('Google do a roll')); // 'Google'
+  
+//   console.log(findLongestWord('May the force be with you')); // 'force'
+
+// function min(a, b){
+//    if(a<b){
+//       return a;
+//    }else{
+//       return b;
+//    }
+// }
+// console.log(min(10, 30));
+// return  a<b ? a : b;
+
+
+
+
+
+
